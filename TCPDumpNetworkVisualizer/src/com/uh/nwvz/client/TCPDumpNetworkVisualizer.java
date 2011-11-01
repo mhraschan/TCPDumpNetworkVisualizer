3c54e91d2e532530950a602de6d404c0706268b7
Michael Hraschan <michael.hraschan@student.tugraz.at>
2011-11-01 14:37:52 +0100

    Initial import.

---
+++ TCPDumpNetworkVisualizer/src/com/uh/nwvz/client/TCPDumpNetworkVisualizer.java
@@ -0,0 +1,16 @@
+package com.uh.nwvz.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class TCPDumpNetworkVisualizer implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}
